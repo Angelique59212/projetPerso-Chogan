675fa79281bb9669c1fbafff79059d9d2d08df6c
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-08 14:37:44 +0100

    ajout sur js et changement index

--- js/app.js
+++ js/app.js
@@ -1,80 +1,108 @@
 let button = document.getElementById("button");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
+let button6 = document.getElementById("button6");
+let button7 = document.getElementById("button7");
+let button8 = document.getElementById("button8");
+let button9 = document.getElementById("button9");
+let button10 = document.getElementById("button10");
+let button11 = document.getElementById("button11");
+let button12 = document.getElementById("button12");
+let button13 = document.getElementById("button13");
+
 button.style.fontSize = "2rem";
+button.style.width = "25rem";
 button.addEventListener("click", ()=> {
     document.location = "page/page-presentation.html";
 })
 
-let button2 = document.getElementById("button2");
+
 button2.style.fontSize = "2rem";
+button2.style.width = "25rem";
 button2.addEventListener("click", ()=> {
     document.location = "page/parfums.html";
 })
 
-let button3 = document.getElementById("button3");
+
 button3.style.fontSize = "2rem";
+button3.style.width = "25rem";
 button3.addEventListener("click", ()=> {
     document.location = "page/produitMenagers.html";
 })
 
-let button4 = document.getElementById("button4");
+
 button4.style.fontSize = "2rem";
+button4.style.width = "25rem";
 button4.addEventListener("click", ()=> {
     document.location = "page/sols.html";
 })
 
-let button5 = document.getElementById("button5");
+
 button5.style.fontSize = "2rem";
+button5.style.width = "25rem";
 button5.addEventListener("click", ()=> {
     document.location = "page/gammes-cosmetique.html";
 })
 
-let button6 = document.getElementById("button6");
+
 button6.style.fontSize = "2rem";
+button6.style.width = "25rem";
 button6.addEventListener("click", ()=> {
     document.location = "page/lait-d-anesse.html";
 })
 
-let button7 = document.getElementById("button7");
+
 button7.style.fontSize = "2rem";
+button7.style.width = "25rem";
 button7.addEventListener("click", ()=> {
     document.location = "page/aloe_vera.html";
 })
 
-let button8 = document.getElementById("button8");
+
 button8.style.fontSize = "2rem";
+button8.style.width = "25rem";
 button8.addEventListener("click", ()=> {
     document.location = "page/bave-escargot.html";
 })
 
-let button9 = document.getElementById("button9");
+
 button9.style.fontSize = "2rem";
+button9.style.width = "25rem";
 button9.addEventListener("click", ()=> {
     document.location = "page/argan.html";
 })
 
-let button10 = document.getElementById("button10");
+
 button10.style.fontSize = "2rem";
+button10.style.width = "25rem";
 button10.addEventListener("click", ()=> {
     document.location = "page/neem.html";
 })
 
-let button11 = document.getElementById("button11");
+
 button11.style.fontSize = "2rem";
+button11.style.width = "25rem";
 button11.addEventListener("click", ()=> {
     document.location = "page/remedy-complex.html";
 })
 
-let button12 = document.getElementById("button12");
+
 button12.style.fontSize = "2rem";
+button12.style.width = "25rem";
 button12.addEventListener("click", ()=> {
     document.location = "page/formulaire.html";
 })
 
-let button13 = document.getElementById("button13");
+
 button13.style.fontSize = "2rem";
+button13.style.width = "25rem";
 button13.addEventListener("click", ()=> {
     document.location = "https://www.chogangroupspa.com/chogangroup/registration/1/AND110";
 })
+
+
 
 
 
