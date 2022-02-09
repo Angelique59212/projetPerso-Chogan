0fb6e8d6caea25338c84149eaa413c491ddbea83
angel <dehainaut.angelique@orange.fr>
2022-02-09 10:27:09 +0100

    class creation for css and js buttons

--- index.php
+++ index.php
@@ -17,55 +17,55 @@ require __DIR__ .'/partials/header.php'?>
 
     <div class="container">
         <div class="position"> <!-- Lien vers ma page de présentation -->
-            <input type="button" id="button" value="Ma présentation">
+            <input type="button" class="buttonStyle" id="button" value="Ma présentation">
         </div>
 
         <div class="position"> <!-- Lien vers parfums -->
-            <input type="button" id="button2" value="Parfums">
+            <input type="button" class="buttonStyle" id="button2" value="Parfums">
         </div>
 
         <div class="position"> <!-- Lien vers produits ménagers -->
-            <input type="button" id="button3" value="Produits ménagers">
+            <input type="button" class="buttonStyle" id="button3" value="Produits ménagers">
         </div>
 
         <div class="position"> <!-- Produits sol -->
-            <input type="button" id="button4" value="Produits sol">
+            <input type="button" class="buttonStyle" id="button4" value="Produits sol">
         </div>
 
         <div class="position"> <!-- Présentation cosmétique -->
-            <input type="button" id="button5" value="Présentation Cosmétique">
+            <input type="button" class="buttonStyle" id="button5" value="Présentation Cosmétique">
         </div>
 
         <div class="position"><!-- Gamme Lait d'anesse -->
-            <input type="button" id="button6" value="Gamme Anesse">
+            <input type="button" class="buttonStyle" id="button6" value="Gamme Anesse">
         </div>
 
         <div class="position"><!-- Gamme Aloe Vera -->
-            <input type="button" id="button7" value="Gamme Aloe Vera">
+            <input type="button" class="buttonStyle" id="button7" value="Gamme Aloe Vera">
         </div>
 
         <div class="position"><!-- Gamme Bave d'escargot -->
-            <input type="button" id="button8" value="Gamme Bave D'escargot">
+            <input type="button" class="buttonStyle" id="button8" value="Gamme Bave D'escargot">
         </div>
 
         <div class="position"> <!-- Gamme Argan -->
-            <input type="button" id="button9" value="Gamme Argan">
+            <input type="button" class="buttonStyle" id="button9" value="Gamme Argan">
         </div>
 
         <div class="position"> <!-- Gamme Neem -->
-            <input type="button" id="button10" value="Gamme Neem">
+            <input type="button" class="buttonStyle" id="button10" value="Gamme Neem">
         </div>
 
         <div class="position"> <!-- Gamme remedy complex -->
-            <input type="button" id="button11" value="Gamme Remedy Complex">
+            <input type="button" class="buttonStyle" id="button11" value="Gamme Remedy Complex">
         </div>
 
         <div class="position"> <!-- Formulaire -->
-            <input type="button" id="button12" value="Formulaire de contact">
+            <input type="button" class="buttonStyle" id="button12" value="Formulaire de contact">
         </div>
 
         <div class="position"> <!-- Boutique en ligne -->
-            <input type="button" id="button13" value="Boutique en ligne">
+            <input type="button" class="buttonStyle" id="button13" value="Boutique en ligne">
         </div>
     </div>
 
--- js/app.js
+++ js/app.js
@@ -1,104 +1,54 @@
-let button = document.getElementById("button");
-let button2 = document.getElementById("button2");
-let button3 = document.getElementById("button3");
-let button4 = document.getElementById("button4");
-let button5 = document.getElementById("button5");
-let button6 = document.getElementById("button6");
-let button7 = document.getElementById("button7");
-let button8 = document.getElementById("button8");
-let button9 = document.getElementById("button9");
-let button10 = document.getElementById("button10");
-let button11 = document.getElementById("button11");
-let button12 = document.getElementById("button12");
-let button13 = document.getElementById("button13");
+let buttons = document.getElementsByClassName('buttonStyle');
 
-button.style.fontSize = "2rem";
-button.style.width = "25rem";
-button.addEventListener("click", ()=> {
+buttons[0].addEventListener("click", ()=> {
     document.location = "page/page-presentation.php";
 })
 
-
-button2.style.fontSize = "2rem";
-button2.style.width = "25rem";
-button2.addEventListener("click", ()=> {
+buttons[1].addEventListener("click", ()=> {
     document.location = "page/parfums.php";
 })
 
-
-button3.style.fontSize = "2rem";
-button3.style.width = "25rem";
-button3.addEventListener("click", ()=> {
+buttons[2].addEventListener("click", ()=> {
     document.location = "page/produitMenagers.php";
 })
 
-
-button4.style.fontSize = "2rem";
-button4.style.width = "25rem";
-button4.addEventListener("click", ()=> {
+buttons[3].addEventListener("click", ()=> {
     document.location = "page/sols.php";
 })
 
-
-button5.style.fontSize = "2rem";
-button5.style.width = "25rem";
-button5.addEventListener("click", ()=> {
+buttons[4].addEventListener("click", ()=> {
     document.location = "page/gammes-cosmetique.php";
 })
 
-
-button6.style.fontSize = "2rem";
-button6.style.width = "25rem";
-button6.addEventListener("click", ()=> {
+buttons[5].addEventListener("click", ()=> {
     document.location = "page/lait-d-anesse.php";
 })
 
-
-button7.style.fontSize = "2rem";
-button7.style.width = "25rem";
-button7.addEventListener("click", ()=> {
+buttons[6].addEventListener("click", ()=> {
     document.location = "page/aloe vera.php";
 })
 
-
-button8.style.fontSize = "2rem";
-button8.style.width = "25rem";
-button8.addEventListener("click", ()=> {
+buttons[7].addEventListener("click", ()=> {
     document.location = "page/bave-escargot.php";
 })
 
-
-button9.style.fontSize = "2rem";
-button9.style.width = "25rem";
-button9.addEventListener("click", ()=> {
+buttons[8].addEventListener("click", ()=> {
     document.location = "page/argan.php";
 })
 
-
-button10.style.fontSize = "2rem";
-button10.style.width = "25rem";
-button10.addEventListener("click", ()=> {
+buttons[9].addEventListener("click", ()=> {
     document.location = "page/neem.php";
 })
 
-
-button11.style.fontSize = "2rem";
-button11.style.width = "25rem";
-button11.addEventListener("click", ()=> {
+buttons[10].addEventListener("click", ()=> {
     document.location = "page/remedy-complex.php";
 })
 
-
-button12.style.fontSize = "2rem";
-button12.style.width = "25rem";
-button12.addEventListener("click", ()=> {
+buttons[11].addEventListener("click", ()=> {
     document.location = "page/formulaire.php";
 })
 
-
-button13.style.fontSize = "2rem";
-button13.style.width = "25rem";
-button13.addEventListener("click", ()=> {
+buttons[12].addEventListener("click", ()=> {
     document.location = "https://www.chogangroupspa.com/chogangroup/registration/1/AND110";
 })
 
